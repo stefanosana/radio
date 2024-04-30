61b5d3f023e23ab7bcdfa578d4f47c40cfe8137e
Stefano Sana <91429062+stefanosana@users.noreply.github.com>
2024-04-30 09:18:33 +0000

    init

---
+++ radio/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
